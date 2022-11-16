9dbba417efae7189493d0c80f8131bf5b0b4914d
Markel Lomana <lomana.markel@uni.eus>
2022-11-16 13:27:04 +0100

    Zerbitzuak

---
+++ Zerbitzuak/src/zerbitzuak/Exekutagarria.java
@@ -0,0 +1,44 @@
+
+package zerbitzuak;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+/**
+ *
+ * @author lomana.markel
+ */
+public class Exekutagarria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Konektau h1=new Konektau(1); //zabaldu odoo
+        Konektau h2=new Konektau(0); //logeau bertan
+
+        Scanner in = new Scanner(System.in);
+        String B_E;//hau eztoi
+
+        while (true){
+            System.out.println("Programa hasi nahi duzu? B/E");
+            B_E=in.next();
+            System.out.println(B_E);
+
+            if (Objects.equals(B_E, "B")){ 
+                System.out.println("Sartu da");
+                break;
+            }
+        }
+        h1.start(); //prozesua hasteko
+        h2.start();
+
+        try{
+            h1.join();
+            h2.join();
+        } catch (InterruptedException ex){
+            ex.printStackTrace();
+        }
+    }
+    
+}
---
+++ Zerbitzuak/src/zerbitzuak/Konektau.java
@@ -0,0 +1,65 @@
+package zerbitzuak;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+/**
+ *
+ * @author lomana.markel
+ */
+public class Konektau extends Thread{
+    String mezua;
+    Runtime r = Runtime.getRuntime();
+        String comando = "cmd /c start firefox 192.168.65.13:8069/web/login/";
+        final Process[] p = {null};
+
+    Konektau(int i) {
+        if(i==1){
+            mezua="h1prozesua";
+        } else {
+            mezua="h2prozesua";
+        }
+        setName(mezua);
+    }
+
+    @Override
+    public void run(){
+        while (true)
+        {
+            System.out.println(mezua);
+            if(mezua.equals("h1prozesua")){
+                //hemen sartu Odoon sartzeko bidan kodigu
+                try {
+                    p[0] = r.exec(comando);
+                    InputStream is = p[0].getInputStream();
+                    BufferedReader br = new BufferedReader
+                            (new InputStreamReader(is));
+                    String linea;
+                    while ((linea = br.readLine()) != null)
+                        System.out.println(linea);
+                    br.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                break; //prozesu hilttenda holan. Hil einbida?
+            }
+            //System.out.println(mezua);
+            if(mezua.equals("h2prozesua")){
+                //hemen sartu odoon logeetako bidan kodigu
+
+                break; //login
+            }
+
+
+            try {
+                sleep(100);
+            } catch (InterruptedException ex) {
+               ex.printStackTrace();
+            }
+        }
+    }
+}
