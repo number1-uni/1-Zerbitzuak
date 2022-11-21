f3979232448cdc6a78db3638cde494caaf6a7ec4
Markel Lomana <lomana.markel@uni.eus>
2022-11-21 11:54:50 +0100

    Aukezpenerako prestatu

--- Zerbitzuak/src/zerbitzuak/Exekutagarria.java
+++ Zerbitzuak/src/zerbitzuak/Exekutagarria.java
@@ -1,4 +1,3 @@
-
 package zerbitzuak;
 
 import java.util.Objects;
@@ -14,31 +13,33 @@ public class Exekutagarria {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Konektau h1=new Konektau(1); //zabaldu odoo
-        Konektau h2=new Konektau(0); //logeau bertan
+        Konektau h1 = new Konektau(1); //zabaldu odoo
+        Konektau h2 = new Konektau(0); //logeau bertan
 
         Scanner in = new Scanner(System.in);
         String B_E;//hau eztoi
 
-        while (true){
-            System.out.println("Programa hasi nahi duzu? B/E");
-            B_E=in.next();
-            System.out.println(B_E);
+        while (true) {
+            System.out.print("Programa hasi nahi duzu? B/E ");
+            B_E = in.next().toUpperCase();
 
-            if (Objects.equals(B_E, "B")){ 
+            if (Objects.equals(B_E, "B")) {
                 System.out.println("Sartu da");
                 break;
+            } else if(Objects.equals( B_E, "E")){
+                System.out.println("Eskerrik asko programa erabiltzearren");
+                System.exit(0);
             }
         }
         h1.start(); //prozesua hasteko
         h2.start();
 
-        try{
+        try {
             h1.join();
             h2.join();
-        } catch (InterruptedException ex){
+        } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
     }
-    
+
 }
--- Zerbitzuak/src/zerbitzuak/Konektau.java
+++ Zerbitzuak/src/zerbitzuak/Konektau.java
@@ -5,60 +5,57 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-
 /**
  *
  * @author lomana.markel
  */
-public class Konektau extends Thread{
+public class Konektau extends Thread {
+
     String mezua;
     Runtime r = Runtime.getRuntime();
-        String comando = "cmd /c start firefox 192.168.65.13:8069/web/login/";
-        final Process[] p = {null};
+    String comando = "cmd /c start firefox 192.168.65.13:8069/web/login/";
+    final Process[] p = {null};
 
     Konektau(int i) {
-        if(i==1){
-            mezua="h1prozesua";
+        if (i == 1) {
+            mezua = "h1prozesua";
         } else {
-            mezua="h2prozesua";
+            mezua = "h2prozesua";
         }
         setName(mezua);
     }
 
     @Override
-    public void run(){
-        while (true)
-        {
-            System.out.println(mezua);
-            if(mezua.equals("h1prozesua")){
+    public void run() {
+        while (true) {
+            if (mezua.equals("h1prozesua")) {
                 //hemen sartu Odoon sartzeko bidan kodigu
                 try {
                     p[0] = r.exec(comando);
                     InputStream is = p[0].getInputStream();
-                    BufferedReader br = new BufferedReader
-                            (new InputStreamReader(is));
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
                     String linea;
-                    while ((linea = br.readLine()) != null)
+                    while ((linea = br.readLine()) != null) {
                         System.out.println(linea);
+                    }
                     br.close();
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
 
-                break; //prozesu hilttenda holan. Hil einbida?
+                break;
             }
             //System.out.println(mezua);
-            if(mezua.equals("h2prozesua")){
+            if (mezua.equals("h2prozesua")) {
                 //hemen sartu odoon logeetako bidan kodigu
 
                 break; //login
             }
 
-
             try {
                 sleep(100);
             } catch (InterruptedException ex) {
-               ex.printStackTrace();
+                ex.printStackTrace();
             }
         }
     }
